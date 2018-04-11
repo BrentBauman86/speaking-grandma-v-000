948d050762dead594b6971638a4560c50bf30725
Brent Bauman <brentbauman86@gmail.com>
2018-04-11 20:58:12 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
-# Write a speak_to_grandma method.
-def speak_to_grandma(phrase = " ")
-  if speak_to_grandma != phrase.uppercase
+phrase = " "
+def speak_to_grandma(phrase)
+  if phrase != phrase.uppercase
     puts "HUH?! SPEAK UP, SONNY!"
 end
 end
