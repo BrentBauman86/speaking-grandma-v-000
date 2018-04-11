a2bb867700523f3b33e559118f24dd7264e19e17
Brent Bauman <brentbauman86@gmail.com>
2018-04-11 20:26:50 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,9 +3,9 @@ def speak_to_grandma(phrase)
   if phrase != phrase.uppercase
   puts "#{HUH?! SPEAK UP, SONNY!}"
 elsif phrase == phrase.uppercase
-  puts "NO, NOT SINCE 1938!"
+  puts "#{NO, NOT SINCE 1938!}"
 else phrase == "I LOVE YOU GRANDMA!"
-  puts "I LOVE YOU TOO PUMPKIN!"
+  puts "#{I LOVE YOU TOO PUMPKIN!}"
 end
 end
 # If you shout, she can hear you (or at least she thinks so)
