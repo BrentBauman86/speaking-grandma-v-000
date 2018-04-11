2f603fa8ef47672773d079536283a45f399e1290
Brent Bauman <brentbauman86@gmail.com>
2018-04-11 21:51:10 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,6 @@
 
 def speak_to_grandma(phrase = " ")
-  if phrase == "hi Nana, how are you?".uppercase
-     "NO, NOT SINCE 1938!"
-
-  else phrase == "I LOVE YOU GRANDMA!"
+  if phrase == "I LOVE YOU GRANDMA!"
      "I LOVE YOU TOO PUMPKIN!"
 end
 end
