8049771e41525bb7182d8828dc2d9a775e84d5cf
Brent Bauman <brentbauman86@gmail.com>
2018-04-11 21:37:26 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,10 @@
-phrase =
+phrase = "I LOVE YOU GRANDMA!"
 def speak_to_grandma(phrase)
   if phrase != phrase.uppercase
-    puts "#{phrase}"
-  elsif phrase == phrase.
-
+    puts "HUH?! SPEAK UP SONNY!"
+  elsif phrase == phrase.uppercase
+    puts "NO, NOT SINCE 1938!"
+  else phrase == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
 end
 end
