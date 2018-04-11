efe14e742070aa5a02bd64741e1be36b55ecb103
Brent Bauman <brentbauman86@gmail.com>
2018-04-11 22:31:07 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 def speak_to_grandma(phrase)
 if phrase == "I LOVE YOU GRANDMA!"
      "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == "WHAT DID YOU EAT TODAY?"
+  elsif phrase == phrase.upcase
     "NO, NOT SINCE 1938!"
-  else phrase == phrase.upcase
+  else phrase != phrase.upcase
       "HUH?! SPEAK UP, SONNY!"
 end
 end
