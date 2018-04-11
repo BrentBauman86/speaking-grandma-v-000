77144f823c91bfc554cde710091c4a3129e7c5d7
Brent Bauman <brentbauman86@gmail.com>
2018-04-11 21:03:42 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 
 def speak_to_grandma(phrase = " ")
-  if speak_to_grandma != phrase.uppercase
+  if " " != " ".uppercase
     puts "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.uppercase
     puts "NO, NOT SINCE 1938!"
