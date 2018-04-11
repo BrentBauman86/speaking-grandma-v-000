d82d630b8b0633b2a6c00ab9d9083f80dcdfbabd
Brent Bauman <brentbauman86@gmail.com>
2018-04-11 22:01:04 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(phrase = " ")
   if phrase == "Hi Nana, how are you?".upcase
     "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == "what did you eat today?".upcase
+  elsif phrase == "WHAT DID YOU EAT TODAY?"
     "NO, NOT SINCE 1938!"
   else phrase == "I LOVE YOU GRANDMA!"
        "I LOVE YOU TOO PUMPKIN!"
