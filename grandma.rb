35f3f7fffea6df5c7f3d1503f4524277852575ad
Brent Bauman <brentbauman86@gmail.com>
2018-04-11 22:08:51 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ if phrase == "I LOVE YOU GRANDMA!"
      "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == "WHAT DID YOU EAT TODAY?"
     "NO, NOT SINCE 1938!"
-  else phrase != "Hi Nana, how are you?".upcase
+  else phrase == "Hi Nana, how are you?".upcase
       "HUH?! SPEAK UP, SONNY!"
 end
 end
